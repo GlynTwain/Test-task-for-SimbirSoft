97537e32737fac0573ee5a4ec4df9738552e4fb5
Shityukov V <tri.dryga.new@gmail.com>
2021-10-10 22:17:42 +0300

    add driver win

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 
 @pytest.fixture(scope="session")
 def browser():
-    path_driver = os.path.abspath('../drivers/chromedriver')
+    path_driver = os.path.abspath('../drivers/chromedriver')  # Используеммая версия - 94.0.4606.61
     driver = webdriver.Chrome(executable_path=path_driver)
     yield driver
     driver.close()
